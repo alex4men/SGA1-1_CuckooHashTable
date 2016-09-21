29ca5e7de1273791d003af53c1959e85fd66ece4
alex <xanderfomenko@gmail.com>
2016-09-21 09:38:20 +0300

    First working and formatted but yet smelling draft

--- src/CuckooHashTable.java
+++ src/CuckooHashTable.java
@@ -16,21 +16,19 @@ import java.util.Random;
 // void clearTable( )      --> Remove all items
 // int  size( )           --> Return number of items
 
-public class CuckooHashTable<K>
-{
+public class CuckooHashTable<K> {
 
-    public CuckooHashTable(int tableSize)
-    {
-        array = (K[][]) new Object[NUM_OF_ARRAYS][tableSize];
-        currentSize = new int[NUM_OF_ARRAYS];
-        clearTable( );
+    public CuckooHashTable(int tableSize) {
+        array = (K[][]) new Object[numOfArrays][tableSize];
+        currentSize = new int[numOfArrays];
+        clearTable();
     }
 
-    private Random r = new Random( );
+    private Random r = new Random();
 
-    private static final int NUM_OF_ARRAYS = 2;
-    private static final double MAX_LOAD = 0.50;
-    private static final int ALLOWED_REHASHES = 1;
+    private final int numOfArrays = 2;
+    private final double maxLoadFactor = 0.50;
+    private final int rehashThrshld = 1;
 
     private int rehashes = 0;
 
@@ -69,7 +67,7 @@ public class CuckooHashTable<K>
 //                x = tmp;
 //            }
 //
-//            if( ++rehashes > ALLOWED_REHASHES )
+//            if( ++rehashes > rehashThrshld )
 //            {
 //                expand( );      // Make the table bigger
 //                rehashes = 0;
@@ -98,7 +96,7 @@ public class CuckooHashTable<K>
 //                    x = tmp;
 //            }
 //
-//            if( ++rehashes > ALLOWED_REHASHES )
+//            if( ++rehashes > rehashThrshld )
 //            {
 //                expand( );      // Make the table bigger
 //                rehashes = 0;
@@ -111,31 +109,30 @@ public class CuckooHashTable<K>
     /**
      * Insert into the hash table. If the item is
      * already present, return false.
+     *
      * @param x the item to insert.
      */
-    public boolean insert(K x)
-    {
-        if(contains(x))
+    public boolean insert(K x) {
+        if (contains(x))
             return false;
 
-        if(currentSize[0] >= array[0].length * MAX_LOAD || currentSize[1] >= array[1].length * MAX_LOAD)
-            expand( );
+        if (currentSize[0] >= array[0].length * maxLoadFactor || currentSize[1] >= array[1].length * maxLoadFactor)
+            expand();
 
         if (array[0][hash(x, 0)] == null) {
             array[0][hash(x, 0)] = x;
             currentSize[0]++;
-            System.out.println("Inserted in 1 array");
+            System.out.println("Inserted in 1st array");
         } else if (array[1][hash(x, 1)] == null) {
             array[1][hash(x, 1)] = x;
             currentSize[1]++;
-            System.out.println("Inserted in 2 array");
+            System.out.println("Inserted in 2nd array");
         } else return false;
 
         return true;
     }
 
-    private int hash(K x, int arrayNum)
-    {
+    private int hash(K x, int arrayNum) {
         if (arrayNum == 0) {
             int hashVal = x.hashCode();
             hashVal = (hashVal & 0x7fffffff) % array[arrayNum].length;
@@ -149,9 +146,9 @@ public class CuckooHashTable<K>
         }
     }
 
-    private void expand( )
-    {
-//        rehash( (int) ( array.length / MAX_LOAD ) );
+    // TODO: 20/09/16
+    private void expand() {
+//        rehash( (int) ( array.length / maxLoadFactor ) );
     }
 //
 //    private void rehash( )
@@ -175,33 +172,34 @@ public class CuckooHashTable<K>
 
     /**
      * Gets the size of the table.
+     *
      * @return number of items in the hash table.
      */
-    public int size( )
-    {
+    public int size() {
         return currentSize[0] + currentSize[1];
     }
 
     /**
      * Gets the length (potential capacity) of the table.
+     *
      * @return length of the internal array in the hash table.
      */
-    public int capacity( )
-    {
+    public int capacity() {
         return array[0].length + array[1].length;
     }
 
     /**
      * Method that searches all hash function places.
+     *
      * @param x the item to search for.
      * @return the position where the search terminates, or -1 if not found.
      */
-    private int findPos( K x )
-    {
-        for (int i = 0; i < NUM_OF_ARRAYS; i++) {
+    private int findPos(K x) {
+        for (int i = 0; i < numOfArrays; i++) {
             int pos = hash(x, i);
             if (array[i][pos] != null && array[i][pos].equals(x)) {
-                System.out.println("Position is: " + i + "." + pos);
+                int tmp = i + 1;
+                System.out.println("Position is: " + tmp + "." + pos);
                 return (i + 1) * pos;
             }
         }
@@ -211,16 +209,15 @@ public class CuckooHashTable<K>
 
     /**
      * Remove from the hash table.
+     *
      * @param x the item to remove.
      * @return true if item was found and removed
      */
-    public boolean remove( K x )
-    {
-        int pos = findPos( x );
+    public boolean remove(K x) {
+        int pos = findPos(x);
 
-        if( pos != -1 )
-        {
-            array[pos / array.length][pos % NUM_OF_ARRAYS] = null;
+        if (pos != -1) {
+            array[pos / array.length][pos % numOfArrays] = null;
             currentSize[pos / array.length]--;
         }
 
@@ -229,22 +226,21 @@ public class CuckooHashTable<K>
 
     /**
      * Find an item in the hash table.
+     *
      * @param x the item to search for.
      * @return the matching item.
      */
-    public boolean contains( K x )
-    {
-        return findPos( x ) != -1;
+    public boolean contains(K x) {
+        return findPos(x) != -1;
     }
 
     /**
      * Make the hash table logically empty.
      */
-    public void clearTable( )
-    {
+    public void clearTable() {
         currentSize[0] = 0;
         currentSize[1] = 0;
-        for( int i = 0; i < array.length; i++ ) {
+        for (int i = 0; i < array.length; i++) {
             array[0][i] = null;
             array[1][i] = null;
         }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main( String [ ] args )
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         CuckooHashTable<String> H = new CuckooHashTable<>(100);
 
